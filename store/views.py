b3e24419875901f249c943be3615e8a9563f7bb5
Thanh Tung <nguyentnt126@gmail.com>
2025-04-15 23:30:41 +0700

    Add register acc func

--- store/views.py
+++ store/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import redirect, render
 from .models import Product
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
+from django import forms
 
 def home(request):
     products = Product.objects.all()
@@ -29,3 +33,22 @@ def logout_user(request):
     logout(request)
     messages.success(request, ("You have been logged out!"))
     return redirect('home')
+
+def register_user(request):
+    form = SignUpForm()
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            #login user
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request,("You have registered successfully!"))
+            return redirect('home')
+        else:
+            messages.success(request, ("Ooopss there is something worng..."))
+            return redirect('register')
+    else:
+        return render(request, 'register.html', {'form': form})
