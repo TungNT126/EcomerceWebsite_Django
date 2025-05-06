c5cee749642ee0be2ee8986ab1536d5351fee21e
Thanh Tung <nguyentnt126@gmail.com>
2025-05-06 21:12:31 +0700

    Delete button and total price in cart

--- cart/cart.py
+++ cart/cart.py
@@ -25,6 +25,26 @@ class Cart():
 
         self.session.modified = True
 
+    def cart_totals(self):
+        # Get prod ID
+        product_ids = self.cart.keys()
+        # Look up keys in DB
+        products = Product.objects.filter(id__in=product_ids)
+        # Get quantitites
+        quantities = self.cart
+        # Start counting at 0
+        total = 0
+        for key, value in quantities.items():
+            key = int(key)
+            for product in products:
+                if product.id == key:
+                    if product.is_sale:
+                        total = total + (product.sale_price * value)
+                    else:
+                        total = total + (product.price * value)
+        return total
+
+
     def __len__(self):
         return len(self.cart)
     
@@ -54,3 +74,12 @@ class Cart():
 
         thing = self.cart
         return thing
+    
+    def delete(self, product):
+        product_id = str(product)
+        # Delete from dic
+        if product_id in self.cart:
+            del self.cart[product_id]
+        
+        self.session.modified = True
+        
--- cart/views.py
+++ cart/views.py
@@ -8,8 +8,9 @@ def cart_summary(request):
     cart = Cart(request)
     cart_products = cart.get_prods
     quantities = cart.get_quants
+    totals = cart.cart_totals()
     
-    return render(request, 'cart_summary.html', {"cart_products":cart_products, "quantities":quantities})
+    return render(request, 'cart_summary.html', {"cart_products":cart_products, "quantities":quantities, "totals":totals})
 
 def cart_add(request):
     # Get the cart
@@ -33,8 +34,16 @@ def cart_add(request):
         response = JsonResponse({'qty': cart_quantity})
         return response
 
-def cart_delete(request_):
-    pass
+def cart_delete(request):
+    cart = Cart(request)
+    if request.POST.get('action') == 'post':
+        #Get stuff
+        product_id = int(request.POST.get('product_id'))
+        #Call delete func in cart
+        cart.delete(product=product_id)
+
+        response = JsonResponse({'product':product_id})
+        return response
 
 def cart_update(request):
     cart = Cart(request)
